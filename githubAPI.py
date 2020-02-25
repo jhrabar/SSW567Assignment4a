0d02340613e50f15ae131b55348b3399a977076b
John <jhrabar@stevens.edu>
2020-02-25 03:45:39 -0500

    updated test cases to use mocks

--- githubAPI.py
+++ githubAPI.py
@@ -2,12 +2,20 @@
 # John Hrabar
 # I Pledge My Honor That I Have Abided By The Stevens Honor System
 
+#John Hrabar
+#I Pledge My Honor That I Have Abided By The Stevens Honor System
+#I had to split my original code from assignment 4 into multiple functions.
+#The core of the code is the same, but it was very difficult to test using mocks
+#when it was all in one function due to the multiple different API calls
 
 import requests
 import json
 
+def getCommitNumber(username, repo):
+	repoResponse = requests.get("https://api.github.com/repos/" + username + "/" + repo + "/commits")
+	return len(repoResponse.json())
 
-def getGithubInfo(username):
+def getGithubRepos(username):
 	if(not username or not isinstance(username, str)):
 		return "Error: Username must be a valid, non-empty string"
 
@@ -16,13 +24,18 @@ def getGithubInfo(username):
 	if(response.status_code != 200):
 		return "Error: Data not found"
 
-	finalResult = ""
-
 	if(len(response.json()) < 1):
-		finalResult = "User " + username + " has no repositories or does not exist"
-	else:
-		for repo in response.json():
-			repoResponse = requests.get("https://api.github.com/repos/" + username + "/" + repo['name'] + "/commits")
-			finalResult = finalResult + "Repo: " + repo['name'] + " Number of Commits: " + str(len(repoResponse.json())) + "\n"
-	
+		return "User " + username + " has no repositories or does not exist"
+
+	return response.json()
+
+
+def getTotalCommitNumber(username):
+	repositories = getGithubRepos(username)
+	if(isinstance(repositories, str)):
+		return repositories
+	finalResult = ""
+	for repo in repositories:
+			commits = getCommitNumber(username, repo['name'])
+			finalResult = finalResult + "Repo: " + repo['name'] + " Number of Commits: " + str(commits) + "\n"
 	return finalResult
--- testGithubAPI.py
+++ testGithubAPI.py
@@ -1,21 +1,46 @@
 
 
 import unittest
+from unittest import mock
+import requests
+import json
 
-from githubAPI import getGithubInfo
+from githubAPI import getGithubRepos, getCommitNumber, getTotalCommitNumber
+
+
 
 class TestGithubAPI(unittest.TestCase):
 
-	def testUsernameInput(self):
-		self.assertEqual("Error: Username must be a valid, non-empty string", getGithubInfo(""))
-		self.assertEqual("Error: Username must be a valid, non-empty string", getGithubInfo(123))
+	@mock.patch('requests.get')
+	def testUsernameInput(self, mockedReq):
+		mockedReq = mock.MagicMock(return_value = {"message": "Not Found","documentation_url": "https://developer.github.com/v3"})
+		self.assertEqual("Error: Username must be a valid, non-empty string", getGithubRepos(""))
+		self.assertEqual("Error: Username must be a valid, non-empty string", getGithubRepos(123))
 
-	def testValidGithubUser(self):
-		self.assertEqual("User fakeuser123456 has no repositories or does not exist", getGithubInfo("fakeuser123456"))
-		self.assertEqual("Error: Data not found", getGithubInfo("fakeuser123 456"))
+	@mock.patch('requests.get')
+	def testValidGithubUser(self, mockedReq):
+		mockedReq.return_value = mock.Mock(status_code = 200)
+		mockedReq.return_value.json.return_value = {}
+		result = getGithubRepos("fakeuser123456")
+		self.assertEqual("User fakeuser123456 has no repositories or does not exist", result)
 
-	def testCommitCount(self):
-		self.assertEqual("Repo: InterviewPrep Number of Commits: 26\nRepo: PersonalWebsite Number of Commits: 15\n",getGithubInfo("MarkSwanson4887"))
+	@mock.patch('requests.get')
+	def testValidGithubUser2(self, mockedReq):
+		mockedReq = mock.MagicMock(return_value = {"message": "Not Found","documentation_url": "https://developer.github.com/v3/repos/#list-user-repositories"})
+		self.assertEqual("Error: Data not found", getGithubRepos("fakeuser123 456"))
+
+	@mock.patch('requests.get')
+	def testCommitNumber(self, mockedReq):
+		mockedReq.return_value = mock.Mock(status_code = 200)
+		mockedReq.return_value.json.return_value = [{"commit1":"one"},{"commit2":"two"}, {"commit3":"three"}, {"commit4":"four"}, {"commit5":"five"}]
+		self.assertEqual(5, getCommitNumber("fakeaccountjohn", "repo1"))
+
+	@mock.patch('requests.get')
+	def testCommitNumber2(self, mockedReq):
+		mockedReq.return_value = mock.Mock(status_code = 200)
+		mockedReq.return_value.json.return_value = {"commit1":"one"}
+		self.assertEqual(1, getCommitNumber("fakeaccountjohn", "repo2"))
+
 
 if __name__ == '__main__':
 	print('Running unit tests')
